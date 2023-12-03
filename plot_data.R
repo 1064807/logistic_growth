e70dd960098d8aab1e93720f99f5f69daa07907b
sathvikakrishnan <150150453+sathvikakrishnan@users.noreply.github.com>
2023-12-03 23:02:33 +0000

    plot_data.R

--- plot_data.R
+++ plot_data.R
@@ -1,11 +1,13 @@
+<pre>
+```R
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment2.csv")
 
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
@@ -15,7 +17,17 @@ ggplot(aes(t,N), data = ???) +
   
   theme_bw()
 
-ggplot(aes(t,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_point() +
+  
+  xlab("t") +
+  
+  ylab("y") +
+  
+  theme_bw()
+
+ggplot(aes(t,N), data = growth_data) +
   
   geom_point() +
   
@@ -24,3 +36,20 @@ ggplot(aes(t,???), data = growth_data) +
   ylab("y") +
   
   scale_y_continuous(trans='log10')
+
+#To make a graph showing 
+ggplot(aes(t, N), data = growth_data) +
+
+  geom_point() +
+
+  geom_vline(xintercept = c(400,1000), linetype = "dashed", color = "blue") +
+
+  labs(title = "Logistic Growth Plot",
+       
+       x = "Time (t)",
+       
+       y = "Population Size (N)")+
+
+  scale_y_continuous(trans='log10')
+   ```
+</pre>
